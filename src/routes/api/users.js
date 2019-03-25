6701e9b70a855cfc693fd8e924ef32d23376b12f
Anton Fedorovskyi <a.fedorovskyi@plarium.com>
2019-03-25 10:35:18 +0200

    users from db

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -1,24 +1,14 @@
 
 const express = require('express');
 const router = express.Router();
-
-const users = [
-    {
-        id: 1,
-        firstName: 'User',
-        lastName: 'One',
-    },
-    {
-        id: 2,
-        firstName: 'User',
-        lastName: 'Two',
-    }
-]
+const User = require('../../models/User');
 
 router.route('/').get((req, res) => {
-    res.status(200).json({
-        users: users,
-      });
+    User.find({}, function (err, users) {
+        res.status(200).json({
+            users: users,
+          });
+    });
 });
 
 module.exports = router;
